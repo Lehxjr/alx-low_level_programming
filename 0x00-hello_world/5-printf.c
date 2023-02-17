4a40a93fbc449b38005c4a93836e53d3c2030ced
Lehxjr <joeylexmensah.jm@gmail.com>
2023-02-17 01:35:46 -0800

    cprogramming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Description: this prints specific text using printf
  *
  * Return: Always returns 0 (Success)
- */ 
-int main(void) 
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
