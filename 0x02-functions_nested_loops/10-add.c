6e7007cc66e5abc1a411acad9990743de937ec0e
Lehxjr <joeylexmensah.jm@gmail.com>
2023-02-23 06:13:47 +0000

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * add - adds numbers
+ * @a: an operand
+ * @b: an operand
+ *
+ * Description: this adds two ints
+ *
+ * Return: returns int
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	result (result);
+}
