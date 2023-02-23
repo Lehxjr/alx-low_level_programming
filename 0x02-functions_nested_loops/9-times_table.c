349d35c0169885aaffe17e3179d19b59554712e7
Lehxjr <joeylexmensah.jm@gmail.com>
2023-02-23 06:22:42 +0000

    nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,6 +25,8 @@ void times_table(void)
 			result = first_digit * second_digit;
 
 			if (result <= 9)
+				_putchar(' ');
+			else
 				_putchar('0' + (result / 10));
 
 			_putchar('0' + (result % 10));
